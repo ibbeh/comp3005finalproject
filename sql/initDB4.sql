11bffecba48f33c93e530cb02ea96b6518d2d727
ibbeh <ibraheemrefai47@outlook.com>
2024-04-12 22:55:39 -0400

    initDB4.sql update, almost complete, should only require minor updates

--- sql/initDB4.sql
+++ sql/initDB4.sql
@@ -14,7 +14,7 @@ CREATE TABLE Competitions (
 
 --FINISHED
 CREATE TABLE Seasons (
-    season_id SERIAL PRIMARY KEY,
+    season_id INTEGER PRIMARY KEY,
     season_name VARCHAR(255),
     start_date DATE,
     end_date DATE,
@@ -134,107 +134,146 @@ CREATE TABLE Team_Statistics (
     FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
 );
 
-
+--PENDING APPROVAL
 CREATE TABLE Substitutions (
-    substitution_id SERIAL PRIMARY KEY,
-    substitution_minute INTEGER,
-    substitutiton_period INTEGER,
-    substitution_reason VARCHAR(255),
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_in_id) INTEGER REFERENCES Players(player_id),
-    FOREIGN KEY (player_out_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    player_out_id INTEGER,
+    player_in_id INTEGER,
+    reason VARCHAR(255),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id),
+    FOREIGN KEY (player_out_id) REFERENCES Players(player_id),
+    FOREIGN KEY (player_in_id) REFERENCES Players(player_id)
 );
 
+--PENDING APPROVAL
 CREATE TABLE Goals (
-    goal_id SERIAL PRIMARY KEY,
-    goal_minute INTEGER,
-    goal_type VARCHAR(255)
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
-    FOREIGN KEY (assist_player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    goal_type VARCHAR(255),
+    assist_event_id INTEGER,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id),
+    FOREIGN KEY (assist_event_id) REFERENCES Events(event_id)
 );
 
+--PENDING APPROVAL
 CREATE TABLE Penalties (
-    penalty_id SERIAL PRIMARY KEY,
-    is_scored BOOLEAN
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
-    FOREIGN KEY (goal_id) INTEGER REFERENCES Goals(goal_id),
+    event_id INTEGER PRIMARY KEY,
+    is_scored BOOLEAN,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+
+--PENDING APPROVAL
 CREATE TABLE Saves (
-    save_id SERIAL PRIMARY KEY,
-    save_minute INTEGER
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+
+--PENDING APPROVAL
 CREATE TABLE Tackles (
-    tackle_id SERIAL PRIMARY KEY,
-    tackle_minute INTEGER,
-    success BOOLEAN
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    outcome BOOLEAN,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+--PENDING APPROVAL
 CREATE TABLE Passes (
-    pass_id SERIAL PRIMARY KEY,
+    event_id INTEGER PRIMARY KEY,
     pass_type VARCHAR(255),
-    successful BOOLEAN
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    successful BOOLEAN,
+    length DECIMAL(5,2),
+    angle DECIMAL(5,2),
+    height VARCHAR(255),
+    end_location_x DECIMAL(5,2),
+    end_location_y DECIMAL(5,2),
+    recipient_id INTEGER,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id),
+    FOREIGN KEY (recipient_id) REFERENCES Players(player_id)
 );
 
+
+--PENDING APPROVAL
 CREATE TABLE Throw_Ins (
-    throw_in_id SERIAL PRIMARY KEY,
-    throw_in_minute INTEGER
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+
+--PENDING APPROVAL
 CREATE TABLE Ball_Receipts (
-    receipt_id SERIAL PRIMARY KEY,
-    receipt_minute INTEGER
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+--PENDING APPROVAL
 CREATE TABLE Cards (
-    card_id SERIAL PRIMARY KEY,
+    event_id INTEGER PRIMARY KEY,
     card_type VARCHAR(255),
-    card_minute INTEGER
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+--PENDING APPROVAL
 CREATE TABLE Fouls (
-    foul_id SERIAL PRIMARY KEY,
-    foul_minute INTEGER
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    foul_type VARCHAR(255),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+--PENDING APPROVAL
 CREATE TABLE Offsides (
-    offside_id SERIAL PRIMARY KEY,
-    offside_minute INTEGER
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+
+--PENDING APPROVAL
 CREATE TABLE Shots (
-    shot_id SERIAL PRIMARY KEY,
-    match_id INTEGER REFERENCES Matches(match_id),
-    player_id INTEGER REFERENCES Players(player_id),
-    shot_minute INTEGER,
-    on_target BOOLEAN
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    outcome BOOLEAN,
+    shot_type VARCHAR(255),
+    body_part VARCHAR(255),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
+
+--PENDING APPROVAL
 CREATE TABLE Dribbles (
-    dribble_id SERIAL PRIMARY KEY,
-    dribble_minute INTEGER,
-    success BOOLEAN
-    FOREIGN KEY (match_id) INTEGER REFERENCES Matches(match_id),
-    FOREIGN KEY (player_id) INTEGER REFERENCES Players(player_id),
+    event_id INTEGER PRIMARY KEY,
+    success BOOLEAN,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
+
+
+--PENDING APPROVAL
+CREATE TABLE Events (
+    event_id SERIAL PRIMARY KEY,
+    match_id INTEGER,
+    type VARCHAR(255),
+    period INTEGER,
+    timestamp TIME,
+    minute INTEGER,
+    second INTEGER,
+    team_id INTEGER,
+    player_id INTEGER,
+    location_x DECIMAL(5,2),
+    location_y DECIMAL(5,2),
+    FOREIGN KEY (match_id) REFERENCES Matches(match_id),
+    FOREIGN KEY (team_id) REFERENCES Teams(team_id),
+    FOREIGN KEY (player_id) REFERENCES Players(player_id)
+);
+
+
+--PENDING APPROVAL
+CREATE TABLE Ball_Recovery (
+    event_id INTEGER PRIMARY KEY,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
+
+
+--PENDING APPROVAL
+CREATE TABLE Carries (
+    event_id INTEGER PRIMARY KEY,
+    end_location_x DECIMAL(5,2),
+    end_location_y DECIMAL(5,2),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
